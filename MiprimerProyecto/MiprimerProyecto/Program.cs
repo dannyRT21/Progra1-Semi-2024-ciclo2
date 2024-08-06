c092e50a7979d65a358506000b400e62b5ec1b14
Edwin Daniel Romero Turcios <USSS025824@ugb.edu.sv>
2024-08-05 18:47:03 -0600

    Uso de la estructura de control while

--- MiprimerProyecto/MiprimerProyecto/Program.cs
+++ MiprimerProyecto/MiprimerProyecto/Program.cs
@@ -6,29 +6,31 @@ using System.Threading.Tasks;
 
 namespace MiprimerProyecto
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            //Ejercicio de suma de dos numeros iontroducidos por el usuario.
-            Console.Write("Num 1: ");
-            int num1 = int.Parse(Console.ReadLine());
-            //Ejercicio de pedir el nombre y la edad y dar un saludo
-            Console.Write("Nombre: ");
-            String nombre = Console.ReadLine();
+            string continuar = "s";
+            while (continuar == "s")
+            {
+                Console.Write("Nombre: ");
+                String nombre = Console.ReadLine();
 
-            Console.Write("Num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
-            Console.Write("Edad: ");
-            int edad = int.Parse(Console.ReadLine());
+                Console.Write("Edad: ");
+                if (int.TryParse(Console.ReadLine(), out int edad))
+                {
+                    Console.WriteLine("Hola, {0} bienvenido a Progra I, estas en la mejor epoca {1} de aprender a programar.",
+                        nombre, edad);
 
-            int res = num1 + num2;
-
-            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, res);
-            Console.WriteLine("Hola, {0} bienvenido a Progra I, estas en la mejor epoca {1} de aprender a programar.",
-                nombre, edad);
-
-            Console.ReadLine();
+                    Console.Write("Continuar = s; Salir = cualquier tecla");
+                    continuar = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("La edad debe ser un número.");
+                    continuar = "s";
+                }
+            }
         }
     }
 }
