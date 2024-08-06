8cd3a8def10d3a757920a44d75879dceaa2258d4
Edwin Daniel Romero Turcios <USSS025824@ugb.edu.sv>
2024-08-05 19:01:12 -0600

    determinar segun el numero de dia (1-7) el dia (Sabado a Domingo)

--- MiprimerProyecto/MiprimerProyecto/Program.cs
+++ MiprimerProyecto/MiprimerProyecto/Program.cs
@@ -1,62 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MiprimerProyecto
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string continuar = "s";
+
+        while (continuar.ToLower() == "s")
         {
-            //Ejercicio de pedir el nombre y la edad y dar un saludo
-            string continuar = "s";
-            //Ejercicio: pedir el nombre y la edad y determinar segun la edad la clasificacion en la que se encuentra el usuario.
-        
-            while (continuar == "s")
+            Console.Clear();
+
+            Console.Write("Número de día (1-7): ");
+            int dia = int.Parse(Console.ReadLine());
+
+            switch (dia)
             {
-                Console.Write("Nombre: ");
-                String nombre = Console.ReadLine();
-
-                Console.Write("Edad: ");
-               
-                int edad = int.Parse(Console.ReadLine()); //"5"=> 5
-
-                Console.WriteLine("Hola, {0} bienvenido a Progra I, estas en la mejor epoca con apenas {1} años para aprender a programar.",
-                    nombre, edad);
-                Console.WriteLine("Continuar = s; Salir = cualquier tecla ");
-                string msg = "";
-
-                if (edad < 0 || edad > 125)
-                {
-                    msg = "Por favor, ingrese su edad en un rango de 0 a 125";
-                }
-                else if (edad <= 2)
-                {
-                    msg = "{0}, Eres un bebe";
-                }
-                else if (edad <= 11)
-                {
-                    msg = "{0}, Eres un niño";
-                }
-                else if (edad <= 17)
-                {
-                    msg = "{0}, Eres un adolescente";
-                }
-                else if (edad <= 65)
-                {
-                    msg = "{0},Eres un adulto";
-                }
-                else if (edad <= 125)
-                {
-                    msg = "{0}, Larga vida";
-                }
-                Console.WriteLine(msg, nombre);
-
-                Console.WriteLine("Continuar = s; Salir = Cualquier tecla. ");
-                continuar = Console.ReadLine();
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 2:
+                    Console.WriteLine("Martes");
+                    break;
+                case 3:
+                    Console.WriteLine("Miércoles");
+                    break;
+                case 4:
+                    Console.WriteLine("Jueves");
+                    break;
+                case 5:
+                    Console.WriteLine("Viernes");
+                    break;
+                case 6:
+                    Console.WriteLine("Sábado");
+                    break;
+                case 7:
+                    Console.WriteLine("Domingo");
+                    break;
+                default:
+                    Console.WriteLine("Día inválido");
+                    break;
             }
+
+            Console.Write("¿Desea continuar? (s/n): ");
+            continuar = Console.ReadLine();
         }
     }
 }
