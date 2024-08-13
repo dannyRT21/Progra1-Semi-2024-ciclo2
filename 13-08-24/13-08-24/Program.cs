d0bd0b01a535ba099d1d8ca4fdb5688eee951235
Edwin Daniel Romero Turcios <USSS025824@ugb.edu.sv>
2024-08-13 15:17:27 -0600

    ISR y otras deducciones (Tabla Actual)

--- 13-08-24/13-08-24/Program.cs
+++ 13-08-24/13-08-24/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace miPrimerProyecto
 {
     internal class Program
@@ -12,31 +13,34 @@ namespace miPrimerProyecto
         {
             //Uso de matrices. Calcular el sueldo de una persona.
             double[,] matriz = new double[,] {
-                {0.01,   487.6,  0, 0},
-                {487.61, 642.85, 10,17.48},
-                {642.86, 915.81, 10,32.70},
-                {915.82, 2058.67,20,60.00},
-                {2058.68,9999999,30,288.57},
+                {0.01,   472.00,  0, 0},
+                {472.01, 895.24, 10,17.67},
+                {895.25, 2038.10, 20,60.00},
+                {2038.11,9999999,30,288.57},
             };
             //calculos de prestaciones
             Console.Write("Sueldo: ");
             double sueldo = double.Parse(Console.ReadLine()),
+                afp = sueldo * 6.25 / 100, // Calcula la AFP (6.25% del sueldo)
+                isss = sueldo * 3 / 100; // Calcula el ISSS (3% del sueldo)
 
-                afp = sueldo * 6.25 / 100,
-                isss = sueldo * 3 / 100;
-            isss = sueldo * 3 / 100;
-            sueldo -= afp;
+            // Resta la AFP y el ISSS del sueldo bruto
+            double sueldoNeto = sueldo - afp - isss;
+
+            // Calcula el ISR
             double isr = 0;
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 4; i++)
             {
-                if (sueldo >= matriz[i, 0] && sueldo <= matriz[i, 1])
+                if (sueldoNeto >= matriz[i, 0] && sueldoNeto <= matriz[i, 1])
                 {
-                    isr = (sueldo - (matriz[i, 0] - 0.01)) * matriz[i, 2] / 100 + matriz[i, 3];
+                    isr = (sueldoNeto - matriz[i, 0]) * matriz[i, 2] / 100 + matriz[i, 3];
                 }
             }
-            sueldo -= isss;
-            sueldo -= isr;
-            Console.WriteLine("Sueldo Neto: {0}, AFP: {1}, ISSS: {2}, ISR: {3}", Math.Round(sueldo, 2), Math.Round(afp,2), isss, Math.Round(isr, 2));
+
+            // Resta el ISR del sueldo neto
+            sueldoNeto -= isr;
+
+            Console.WriteLine("Sueldo Neto: {0}, AFP: {1}, ISSS: {2}, ISR: {3}", Math.Round(sueldoNeto, 2), Math.Round(afp, 2), isss, Math.Round(isr, 2));
             Console.ReadLine();
         }
     }
