0698a3aea777c28f628d9f716fbb1a2ee9a5060f
Edwin Daniel Romero Turcios <USSS025824@ugb.edu.sv>
2024-08-13 07:37:36 -0600

    Uso de matrices

---
+++ 13-08-24/13-08-24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_08_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uso de matrices
+            int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
+            Console.WriteLine("El numero en la posicion 3 es {0}", numeros[3]);
+            //pausa
+            Console.ReadKey();
+
+                }
+    }
+}
