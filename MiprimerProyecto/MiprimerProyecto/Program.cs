6d929dd78928a023c815c9f0a3097239b37fce2f
dannyRT21 <USSS025824@ugb.edu.sv>
2024-09-15 21:59:17 -0600

    Parcial1 actividad 2

--- MiprimerProyecto/MiprimerProyecto/Program.cs
+++ MiprimerProyecto/MiprimerProyecto/Program.cs
@@ -1,26 +1,44 @@
 ﻿using System;
 
-namespace miPrimerProyecto
-{
+
     internal class Conversores
+{
+
+  
+    class Program
     {
-        double[][] valores = new double[][] {
-            new double[] {},//vacio solo para comenzar en 1
-            new double[] {0,1,7.73,24.76,36.80, 517.04,8.75,0.9},//monedas
-            new double[] {0,1,100,39.37,3.28084,1.196308,1.09361,0.001},//longitud
-            new double[] {0,1,453.592,16,0.453592, 0.000446429}, //masa
-            new double[] {0,1,86400, 1440, 24, 0.142857, 0.032876643423, 0.0027397232876831892345}//tiempo
-        };
-        public String[][] etiquetas = new String[][]{
-            new String[] {},//vacio solo para comenzar en 1
-            new string[] {"", "Dolar","Quetzal","Lempira","Cordoba","Colon CR","Colon SV", "Euro"},//monedas
-            new string[] {"", "Metro","Cm","Pulgada","Pie","Vara","Yarda","Km"},//longitud
-            new string[] {"", "Libra","Gramo","Onza","Kilogramo","Tonelada Larga"},//Masa
-            new string[] {"", "Dia","Sg","Min","Horas","Semana","Mes","Año"}
-        };
-        public double convertir(int de, int a, double cantidad, int opcion)
+        static void Main(string[] args)
         {
-            return valores[opcion][a] / valores[opcion][de] * cantidad;
+            Console.WriteLine("Calculadora de Impuesto a las Actividades Económicas");
+            Console.WriteLine("---------------------------------------------------");
+
+            Console.WriteLine("Ingrese el monto de la actividad económica:");
+            double monto = double.Parse(Console.ReadLine());
+
+            double impuesto = CalcularImpuesto(monto);
+
+            Console.WriteLine($"El impuesto a pagar es: ${impuesto:F2}");
+
+            Console.WriteLine("Presione una tecla para continuar...");
+            Console.ReadKey();
+        }
+
+        static double CalcularImpuesto(double monto)
+        {
+            double[] rangos = { 0.01, 500.01, 1000.01, 2000.01, 3000.01, 6000, 8000, 18000.01, 30000.01, 60000.01, 100000.01, 200000.01, 300000.01, 400000.01, 500000.01, 10000000.1 };
+            double[] precios = { 1.5, 1.5, 3, 3, 6, 9, 15, 39, 63, 93, 125, 195, 255, 300, 340, 490 };
+            double[] adicionales = { 0, 3, 3, 3, 3, 2, 2, 2, 1, 0.8, 0.7, 0.6, 0.45, 0.4, 0.30, 0.18 };
+
+            int i = 0;
+            while (monto > rangos[i])
+            {
+                i++;
+            }
+
+            double exceso = monto - rangos[i - 1];
+            double impuesto = (exceso / 1000) * adicionales[i - 1] + precios[i - 1];
+
+            return impuesto;
         }
     }
 }
