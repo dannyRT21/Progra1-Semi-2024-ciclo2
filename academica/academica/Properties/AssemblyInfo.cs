cb5bf5b218456e51983bcb81b9c336e83a13494c
dannyRT21 <USSS025824@ugb.edu.sv>
2024-09-23 22:03:24 -0600

    Creacion de la rama Bloque2 y del proyecto academica

--- academica/academica/Properties/AssemblyInfo.cs
+++ academica/academica/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada a un ensamblado.
-[assembly: AssemblyTitle("MiprimerProyecto")]
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("academica")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiprimerProyecto")]
+[assembly: AssemblyProduct("academica")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("c95ed853-c81d-4ea3-854d-4e7e47f2350b")]
+[assembly: Guid("dcb0bce5-49de-4aff-9764-e956770b94f6")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Número de compilación
 //      Revisión
 //
-// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
-// utilizando el carácter "*", como se muestra a continuación:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
