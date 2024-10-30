54f8184dbf9d3da5721b0485e49b3f489be80ac7
dannyRT21 <USSS025824@ugb.edu.sv>
2024-10-29 23:16:28 -0600

    creacion  del index y menu de aplicacion academica y actualizacion de Framework 8.08 a 8.0.10

--- sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
+++ sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1440df4c2afdd61518a2b334bc86497a6d52aeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dcf4c0f480ee7de9fa9ed82b5cff697ccee8970")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
