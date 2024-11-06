f16fde1aee80e160e7d7ffd11d4ce272c86db214
dannyRT21 <USSS025824@ugb.edu.sv>
2024-11-06 08:11:52 -0600

    Terminacion del CRUD de alumnos con API REST FULL de ASP.NET Core + Entity Framework + SQL Server

--- sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
+++ sistema_academico/sistema_academico/obj/Debug/net8.0/sistema_academico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dcf4c0f480ee7de9fa9ed82b5cff697ccee8970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f8184dbf9d3da5721b0485e49b3f489be80ac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema_academico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
